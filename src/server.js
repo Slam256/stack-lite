2ca284f528e2b322ee5cbeeafb4d0420421612a6
Stacey Ali <96086935+Slam256@users.noreply.github.com>
2022-07-27 11:17:53 -0400

    added get routes and updated project structure.

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-  res.end('hello from server');
-}).listen(4001);
-console.log('server is running');
-
-export default server;
