c1dbe793d9cfab9d687c8c70dfb7d1b94571d960
Stacey Ali <96086935+Slam256@users.noreply.github.com>
2022-08-03 11:15:41 -0400

    fix linting errors

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -16,8 +16,9 @@ class QuestionsController {
       const { id } = req.params;
       // check if the question exists in the database
       const question = questions.find((qtn) => qtn.id === parseInt(id, 10));
-      if (!question)
+      if (!question) {
         res.status(404).json({ message: "Question was not found" });
+      }
       res.status(200).json(question);
     } catch (e) {
       console.log(e.message);
